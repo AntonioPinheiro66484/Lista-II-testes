8a08c5602e1d6c69f14ebc2471991936a61d24d9
antonio <ap66484@gmail.com>
2023-12-03 17:20:04 -0300

    Atualizando codigo

--- CasodeUso1.js
+++ CasodeUso1.js
@@ -7,7 +7,6 @@ function gerarInformacoesDisciplina() {
   const locais = ['Sala 101', 'Sala 203', 'Laboratório 1'];
   const codigosTurma = ['T123', 'T456', 'T789'];
 
-<<<<<<< HEAD
   const indiceAleatorio = (array) => Math.floor(Math.random() * array.length);
 
   return {
@@ -126,61 +125,9 @@ function main() {
   
         default:
           console.log('Opção inválida. Tente novamente.');
-=======
-  inscreverAluno(aluno) {
-    if (this.estaDisponivel()) {
-      if (!this.horarioConflita(aluno.disciplinas)) {
-        this.alunosInscritos.push(aluno);
-        this.vagas--;
-        aluno.creditos -= this.creditos;
-        aluno.disciplinas.push(this);
-        return true;
-      } else {
-        //RN00 Choque de horarios
-        console.log("Erro: Conflito de horário.");
-        return false;
->>>>>>> 91bd6a1491034fea22cc03915659b96c48c8f26a
       }
     }
   }
-<<<<<<< HEAD
   
   // Iniciar o programa
   main();
-=======
-
-  horarioConflita(disciplinas) {
-    return disciplinas.some(d => d.horario === this.horario);
-  }
-}
-
-class Aluno {
-  constructor(nome, creditos) {
-    this.nome = nome;
-    this.creditos = creditos;
-    this.disciplinas = [];
-  }
-
-  editarDisciplinas(disciplinas) {
-    this.disciplinas = disciplinas.filter(d => d.inscreverAluno(this));
-  }
-}
-
-// Criar algumas disciplinas de exemplo
-const disciplina1 = new Disciplina("Matemática", "Prof. Tokio", "Segunda-feira", "Sala 101", 4, 20);
-const disciplina2 = new Disciplina("Física", "Prof. Souza", "Terça-feira", "Sala 102", 4, 20);
-const disciplina3 = new Disciplina("Algebra linear", "Prof. Furia", "Quarta-feira", "Sala 201", 4, 20);
-const disciplina4 = new Disciplina("Logica de progracao e algoritmo", "Prof. Eliane", "Quinta-feira", "Sala 201", 4, 20);
-const disciplina5 = new Disciplina("Qualidade de teste e software", "Prof. Vendramel", "Sexta-feira", "Sala 201", 4, 20);
-// Criar um aluno de exemplo
-const aluno1 = new Aluno("João", 20);
-
-// Tentar se inscrever em disciplinas
-disciplina1.inscreverAluno(aluno1);
-disciplina2.inscreverAluno(aluno1);
-disciplina3.inscreverAluno(aluno1);
-disciplina4.inscreverAluno(aluno1);
-disciplina5.inscreverAluno(aluno1);
-
-console.log(aluno1.disciplinas);
->>>>>>> 91bd6a1491034fea22cc03915659b96c48c8f26a

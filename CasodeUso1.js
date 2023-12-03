c060e623a0ed2c9c2c5bb8910b8b158fd35e4d68
antonio <ap66484@gmail.com>
2023-12-03 14:50:14 -0300

    Caso de teste

--- CasodeUso1.js
+++ CasodeUso1.js
@@ -1,86 +1,80 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 class Disciplina {
-  constructor(codigo, nome, preRequisitos) {
-    this.codigo = codigo;
+  constructor(nome, professor, horario, sala, creditos, vagas) {
     this.nome = nome;
-    this.preRequisitos = preRequisitos || [];
+    this.professor = professor;
+    this.horario = horario;
+    this.sala = sala;
+    this.creditos = creditos;
+    this.vagas = vagas;
+    this.alunosInscritos = [];
+  }
+
+  estaDisponivel() {
+    return this.vagas > 0 && this.creditos <= 20;
+  }
+
+  inscreverAluno(aluno) {
+    if (this.estaDisponivel()) {
+      if (!this.horarioConflita(aluno.disciplinas)) {
+        this.alunosInscritos.push(aluno);
+        this.vagas--;
+        aluno.creditos -= this.creditos;
+        aluno.disciplinas.push(this);
+        return true;
+      } else {
+        console.log("Erro: Conflito de horário.");
+        return false;
+      }
+    } else {
+      console.log("Erro: Disciplina indisponível ou sem créditos suficientes.");
+      return false;
+    }
+  }
+
+  horarioConflita(disciplinas) {
+    return disciplinas.some(d => d.horario === this.horario);
   }
 }
 
 class Aluno {
-  constructor(nome, disciplinasCursadas) {
+  constructor(nome, creditos) {
     this.nome = nome;
-    this.disciplinasCursadas = disciplinasCursadas || [];
-    this.inscricoes = [];
+    this.creditos = creditos;
+    this.disciplinas = [];
+  }
+
+  editarDisciplinas(disciplinas) {
+    this.disciplinas = disciplinas.filter(d => d.inscreverAluno(this));
   }
 }
 
-function mostrarDisciplinasDisponiveis() {
-  console.log("Disciplinas Disponíveis:");
-  console.log("1. Matemática");
-  console.log("2. Física");
-  console.log("3. Química");
-}
+// Criar algumas disciplinas de exemplo
+const disciplina1 = new Disciplina("Matemática", "Prof. Silva", "Segunda-feira", "Sala 101", 4, 5);
+const disciplina2 = new Disciplina("Física", "Prof. Souza", "Segunda-feira", "Sala 102", 4, 3);
+const disciplina3 = new Disciplina("Química", "Prof. Santos", "Terça-feira", "Sala 201", 4, 0);
 
-function selecionarDisciplina() {
-  return new Promise(resolve => {
-    rl.question("Escolha a disciplina (1, 2 ou 3): ", opcao => {
-      switch (opcao) {
-        case "1":
-          resolve(disciplina1);
-          break;
-        case "2":
-          resolve(disciplina2);
-          break;
-        case "3":
-          resolve(disciplina3);
-          break;
-        default:
-          console.log("Opção inválida. Tente novamente.");
-          resolve(selecionarDisciplina());
-      }
-    });
-  });
-}
+// Criar um aluno de exemplo
+const aluno1 = new Aluno("João", 20);
+const aluno2 = new Aluno("Igor", 20);
+const aluno3 = new Aluno("Vinicius", 20);
 
-function selecionarAluno() {
-  return new Promise(resolve => {
-    rl.question("Escolha o aluno (1, 2 ou 3): ", opcao => {
-      switch (opcao) {
-        case "1":
-          resolve(aluno1);
-          break;
-        case "2":
-          resolve(aluno2);
-          break;
-        case "3":
-          resolve(aluno3);
-          break;
-        default:
-          console.log("Opção inválida. Tente novamente.");
-          resolve(selecionarAluno());
-      }
-    });
-  });
-}
+// Tentar se inscrever em disciplinas
+disciplina1.inscreverAluno(aluno1);
+disciplina2.inscreverAluno(aluno1);
+disciplina3.inscreverAluno(aluno1);
 
-function realizarInscricao() {
-  mostrarDisciplinasDisponiveis();
+console.log(aluno1.disciplinas);
 
-  selecionarAluno().then(alunoSelecionado => {
-    selecionarDisciplina().then(disciplinaSelecionada => {
-      disciplinaSelecionada.inscreverAluno(alunoSelecionado);
+// Tentar se inscrever em disciplinas
+disciplina1.inscreverAluno(aluno2);
+disciplina2.inscreverAluno(aluno2);
+disciplina3.inscreverAluno(aluno2);
 
-      console.log(`${alunoSelecionado.nome} se inscreveu em ${disciplinaSelecionada.nome}.`);
-      console.log(`Disciplinas de ${alunoSelecionado.nome}:`, alunoSelecionado.disciplinas);
+console.log(aluno2.disciplinas);
 
-      rl.close();
-    });
-  });
-}
+// Tentar se inscrever em disciplinas
+disciplina1.inscreverAluno(aluno3);
+disciplina2.inscreverAluno(aluno3);
+disciplina3.inscreverAluno(aluno3);
+
+console.log(aluno3.disciplinas);
